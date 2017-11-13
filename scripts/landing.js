1d7b1667f5b536665c4e79c420c567def66938df
Anton Shishkin <shishkinanton14@gmail.com>
2017-11-13 21:44:51 +0500

    Рендер несколько паршиалов, наброски меню

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,10 +1,18 @@
 window.onload = function() {
-	var xhr= new XMLHttpRequest();
-	xhr.open('GET', 'header.html', true);
-	xhr.onreadystatechange= function() {
-	    if (this.readyState!==4) return;
-	    if (this.status!==200) return; 
-	    document.getElementById('header').innerHTML= this.responseText;
-	};
-	xhr.send();
+	var partials = ['header.html', 'footer.html', 'side_menu.html'];
+	render_paricles(partials);
+}
+
+//Формат имени паршиала *id элемента для вставки*.html
+function render_paricles(partials) {
+	partials.forEach(function(partial) {
+		var xhr= new XMLHttpRequest();
+		xhr.open('GET', partial, true);
+		xhr.onreadystatechange= function() {
+		    if (this.readyState!==4) return;
+		    if (this.status!==200) return; 
+		    document.getElementById(partial.slice(0, -5)).innerHTML= this.responseText;
+		};
+		xhr.send();	
+	})
 }
