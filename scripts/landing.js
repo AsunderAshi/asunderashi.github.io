ef9a17cf9b9fdce4daea90808f200e7fc1b3450c
Anton Shishkin <shishkinanton14@gmail.com>
2017-11-17 23:19:48 +0500

    .

--- scripts/landing.js
+++ scripts/landing.js
@@ -16,3 +16,20 @@ function render_paricles(partials) {
 		xhr.send();	
 	})
 }
+
+window.onscroll = function() {
+	var scrolled = document.documentElement.scrollTop;
+	var menu_block = document.getElementById("side_menu");
+	if(scrolled > document.getElementById('page_wrapper').offsetHeight - menu_block.offsetHeight + 75) {
+		menu_block.style.position = 'absolute';
+		menu_block.style.top = (document.getElementById('page_wrapper').offsetHeight - menu_block.offsetHeight + 75) + "px";
+	} else {
+		if (scrolled > 77) {		
+			menu_block.style.position = 'fixed';
+			menu_block.style.top = '0px';
+		} else {
+			menu_block.style.position = 'absolute';
+			menu_block.style.top = "78px";
+		}
+	}
+}
